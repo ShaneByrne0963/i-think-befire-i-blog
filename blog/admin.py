26649675b6d3fae8030a4c66513d603039b03761
ShaneByrne0963 <>
2023-10-06 15:09:23 +0000

    Add views and template for index.html page

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -12,5 +12,19 @@ class PostAdmin(SummernoteModelAdmin):
     # Displays the following keys on the admin database list
     list_display = ('title', 'slug', 'status', 'created_on')
     # Allows the admin to search by title or content
-    search_fields = ['title', 'content']
+    search_fields = ('title', 'content')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # Displays the following keys on the admin database list
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    # Allows the admin to filter the database items by these keys
+    list_filter = ('approved', 'created_on')
+    # Allows the admin to search by name, body or email
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
