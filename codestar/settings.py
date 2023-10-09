32b9b4e9d13ac8193783e7b7782d1f8f928b06e1
ShaneByrne0963 <>
2023-10-09 10:55:33 +0000

    Enable Debug to test deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 development = os.environ.get('DEVELOPMENT', 'False')
-DEBUG = development == 'True'
+# DEBUG = development == 'True'
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
